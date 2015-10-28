791cd8c10dbb875830fe0d88982287739e04f7b7
andela-atemitope <amodut@gmail.com>
2015-10-28 15:43:47 +0100

    created .hound.yml file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
