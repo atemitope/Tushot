ed43676fe6549d02f0689cec48b934c4d23417ab
andela-atemitope <amodut@gmail.com>
2015-10-28 15:25:41 +0100

    effected HoundCI corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to '/login' unless current_user
+    redirect_to "/login" unless current_user
   end
 end
