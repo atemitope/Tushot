844527c82d07c14ab9c068657369d359466fb162
andela-atemitope <amodut@gmail.com>
2015-11-10 11:58:08 +0100

    Fixed CircleCI Build Error

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class IndexControllerTest < ActionController::TestCase
+class LinksControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
