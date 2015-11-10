e93067dd755a56116816647aee60e3ccb0e72ae4
andela-atemitope <amodut@gmail.com>
2015-11-10 13:03:11 +0100

    enabled HoundCi.yml

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,16 +1,16 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe Link, type: :model do
-#   subject(:link) { Link.new(long_url: long_url) }
-#   let(:long_url) { "http://www.andela.com" }
+RSpec.describe Link, type: :model do
+  subject(:link) { Link.new(long_url: long_url) }
+  let(:long_url) { "http://www.andela.com" }
 
 
-#   # describe "#generate_short_url" do
+  # describe "#generate_short_url" do
 
-#   #   it "shortens a string if the current value of the short url is nil" do
-#   #    allow(link).to receive(:short_url).and_return(nil)
-#   #       # generate_short_url
-#   #     expect(link.short_url).to receive(:length).and_return(6)
-#   #   end
-#   # end
-# end
+  #   it "shortens a string if the current value of the short url is nil" do
+  #    allow(link).to receive(:short_url).and_return(nil)
+  #       # generate_short_url
+  #     expect(link.short_url).to receive(:length).and_return(6)
+  #   end
+  # end
+end
